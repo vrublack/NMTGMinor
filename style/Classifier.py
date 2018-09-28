11870b7084e9c8496fcd04bd2d0dc77fdaead331
Valentin <valentin.rublack@gmail.com>
2018-09-28 11:04:47 -0400

    Implemented bottleneck option. Using Lstm instead of Linear for classifier, since the representation can have multiple dimensions if bottleneck is false.

--- style/Classifier.py
+++ style/Classifier.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.functional import softmax
@@ -8,13 +9,23 @@ class RepresentationClassifier(nn.Module):
     """
     def __init__(self, input_dim, hidden_dim, classes=2):
         super(RepresentationClassifier, self).__init__()
-        self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, classes)
+        self.lstm = nn.LSTM(input_dim, hidden_dim)
+        self.fc = nn.Linear(hidden_dim, classes)
+        self.hidden_dim = hidden_dim
+
+    def init_hidden(self, size_batch):
+        # num_layers x minibatch_size x hidden_dim
+        return (torch.zeros(1, size_batch, self.hidden_dim),
+                torch.zeros(1, size_batch, self.hidden_dim))
 
     def forward(self, x):
-        # B x T x D
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        # input: (batch x seq_len x input_size) but expected (seq_len x batch x input_size)
+        batch_size = x.shape[0]
+        seq_len = x.shape[1]
+        x = x.transpose(0, 1)
+        # hidden are ideally created only once but the batch size changes
+        hidden = self.init_hidden(x.shape[1])
+        lstm_out, hidden = self.lstm(x, hidden)
+        x = self.fc(lstm_out[-1,:,:])
         x = x.squeeze(1)
-        x.squeeze(1)
         return softmax(x, dim=1)

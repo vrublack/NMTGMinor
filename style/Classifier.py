d31070e4f810ca807a88c55237e115756dbd3fc5
Valentin <valentin.rublack@gmail.com>
2018-11-04 14:57:04 -0500

    -Re-initializing classifier every time when switching to classifier training phase -fixed bug where the other decoder weights were modified when copying them
    (cherry picked from commit 69ac4b8)

--- style/Classifier.py
+++ style/Classifier.py
@@ -11,7 +11,7 @@ class RepresentationClassifier(nn.Module):
     Simple lstm classifier with 1 hidden layer
     """
 
-    def __init__(self, opt, preinit_decoders, dicts, positional_encoder, hidden_dim, dropout=0, classes=2):
+    def __init__(self, opt, dicts, positional_encoder, hidden_dim, dropout=0, classes=2):
         super(RepresentationClassifier, self).__init__()
         self.fc = nn.Linear(opt.model_size, classes)
         self.lstm = nn.LSTM(opt.model_size, hidden_dim)
@@ -21,21 +21,18 @@ class RepresentationClassifier(nn.Module):
         self.cuda = (len(opt.gpus) >= 1)
         self.decoder = TransformerDecoder(opt, dicts, positional_encoder)
 
-        # pre-initialize classifier decoder weights with reconstruction decoder weights
-        self.avg_weights(self.decoder, preinit_decoders)
-
-    def avg_weights(self, target_model, source_models):
+    def avg_weights(self, source_models):
         with torch.no_grad():
-            dict_params = dict(source_models[0].named_parameters())
+            dict_params = {}
 
             for name, param in source_models[0].named_parameters():
-                preinitialized = param
+                preinitialized = param.clone().detach()
                 for other in source_models[1:]:
-                    preinitialized += dict(other.named_parameters())[name]
+                    preinitialized += dict(other.named_parameters())[name].clone().detach()
                 preinitialized_avg = preinitialized / len(source_models)
                 dict_params[name] = preinitialized_avg.detach()
 
-            target_model.load_state_dict(dict_params, strict=False)
+            self.decoder.load_state_dict(dict_params, strict=False)
 
     def init_hidden(self, size_batch):
         # num_layers x minibatch_size x hidden_dim

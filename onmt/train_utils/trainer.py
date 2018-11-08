da4b0ce1fb48ad89a3105b6492e9f656635c4eaf
Valentin <valentin.rublack@gmail.com>
2018-11-08 15:09:55 -0500

    If save_grad isn't specified, the grad isn't saved
    (cherry picked from commit c2bf1ca)

--- onmt/train_utils/trainer.py
+++ onmt/train_utils/trainer.py
@@ -279,7 +279,7 @@ class XETrainer(BaseTrainer):
                     loss_total = total_loss_f(loss_reconstruction, loss_adv)
                     loss_total.backward()
 
-                    if i <= 1:  # only show for first two batches in epoch
+                    if opt.save_grad is not None and i <= 1:  # only show for first two batches in epoch
                         plot_grad_flow(self.model.named_parameters(), '{}_ep_{}_it_{}_{}.png'.format(opt.save_grad, epoch, i, train_phase),
                                        '{}_ep_{}_it_{}_{}.csv'.format(opt.save_grad, epoch, i, train_phase))
 

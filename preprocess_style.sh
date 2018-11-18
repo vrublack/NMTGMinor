a2f989b67cc077ed60fd1a056c36890f90feefa8
Valentin <valentin.rublack@gmail.com>
2018-11-18 17:58:01 -0500

    Slight modification to preprocess script (can pass in name of data type)

--- preprocess_style.sh
+++ preprocess_style.sh
@@ -3,6 +3,7 @@
 input=$1
 name=$2
 lang=$3
+rm=$4
 
 #expected structure: .${data_dir}/${input}/[train or valid]/[train or valid].[0 or 1]
 
@@ -50,12 +51,12 @@ cd $current_dir
 python3 preprocess_style.py \
 -train_style1 ${data_dir}/${name}/train/train.0 \
 -train_style2 ${data_dir}/${name}/train/train.1 \
--train_style1_rm ${data_dir}/${name}/train/train-del.0 \
--train_style2_rm ${data_dir}/${name}/train/train-del.1 \
+-train_style1_rm ${data_dir}/${name}/train/train-del-${rm}.0 \
+-train_style2_rm ${data_dir}/${name}/train/train-del-${rm}.1 \
 -valid_style1 ${data_dir}/${name}/valid/valid.0 \
 -valid_style2 ${data_dir}/${name}/valid/valid.1 \
--valid_style1_rm ${data_dir}/${name}/valid/valid-del.0 \
--valid_style2_rm ${data_dir}/${name}/valid/valid-del.1 \
+-valid_style1_rm ${data_dir}/${name}/valid/valid-del-${rm}.0 \
+-valid_style2_rm ${data_dir}/${name}/valid/valid-del-${rm}.1 \
 -save_data ${data_dir}/${name}/model_prepr
 
 rm -r ${data_dir}/tmp/${name}/
